f093c82d7c2a97edcdaf5a79863c492a96580484
cameo choquer <cjchoquer@gmail.com>
2023-04-02 18:28:11 +0100

    cards for each company and key value pairs show up

--- script.js
+++ script.js
@@ -1,33 +1,84 @@
 const companies = [
+  '',
   'foundersandcoders',
   'orangejellyfish',
   'tldraw',
-  'policyinpractice',
-  'soon_',
+  'policy-in-practice',
+  'thisissoon',
 ]
 
-const orgName = 'foundersandcoders'
+// const selectElement = document.getElementById('companies')
 
-fetch(`https://api.github.com/orgs/${orgName}`)
-  .then((response) => {
-    if (response.status === 404) {
-      throw new Error('Org not found')
-    }
-    return response.json() // Access the response using json() method
-  })
-  .then((org) => {
-    console.log(`this is ${org.name}`)
-  })
+const createNewCard = (company) => {
+  const cardTemplate = document.getElementById('company-card')
+  const card = cardTemplate.content.cloneNode(true)
+  card.querySelector('h1').textContent = company
+}
 
-fetch(`https://api.github.com/orgs/${orgName}/members`)
-  .then((response) => {
-    if (response.status === 404) {
-      throw new Error('Org not found')
-    }
-    return response.json() // Access the response using json() method
+// companies.forEach((company) => {
+//   const optionElement = document.createElement('option')
+//   optionElement.innerText = company
+//   selectElement.appendChild(optionElement)
+// })
+
+function fetchCompanyInfo(company) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://api.github.com/orgs/${company}`)
+      .then((response) => {
+        if (response.status === 404) {
+          throw new Error('Org not found')
+        }
+        return response.json()
+      })
+      .then((org) => {
+        const {
+          name,
+          description,
+          followers,
+          location,
+          email,
+          public_repos,
+          twitter_username,
+        } = org
+        const companyInfo = {
+          name,
+          description,
+          followers,
+          location,
+          email,
+          public_repos,
+          twitter_username,
+        }
+        resolve(companyInfo)
+      })
+      .catch((error) => {
+        reject(error)
+      })
   })
-  .then((members) => {
-    members.forEach((member) => {
-      console.log(member.login)
+}
+const container = document.getElementById('company-container')
+
+Promise.all(companies.slice(1).map((company) => fetchCompanyInfo(company)))
+  .then((companyInfoArray) => {
+    companyInfoArray.forEach((company) => {
+      const card = document.createElement('div')
+      card.className = 'card'
+      const h1 = document.createElement('h1')
+      h1.textContent = company.name
+      card.appendChild(h1)
+
+      Object.keys(company).forEach((key) => {
+        if (key !== 'name' && company[key] !== null) {
+          const p = document.createElement('p')
+          p.textContent = `${key}: ${company[key]}`
+          card.appendChild(p)
+        }
+      })
+
+      container.appendChild(card)
+      console.log(company)
     })
+  })
+  .catch((error) => {
+    console.error(error)
   })
